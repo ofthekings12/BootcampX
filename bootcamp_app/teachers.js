3ed510cfe8075a5b793217eaefd45c13ddecbd83
Mikko De Los Reyes <mikkode212@gmail.com>
2022-01-06 20:01:55 +0000

    parameterized teachers and students queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,16 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2];
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
+WHERE cohorts.name LIKE $1
+ORDER BY cohorts.name;`;
+const values = [`%${cohortName}%`];
+
+
+pool
+ .query(queryString, values)
+ .then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.cohort}: ${user.teacher}`);
   })
 }).catch(err => console.error('query error', err.stack));
